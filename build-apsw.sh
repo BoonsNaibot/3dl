23c8a84666442b6d87e18fe6b71ef2cad01d6cbd
BoonsNaibot <deguzouwa@yahoo.com>
2014-02-24 23:25:30 -0600

    Create build-apsw.sh

---
+++ build-apsw.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. $(dirname $0)/environment.sh
+
+if [ ! -f $CACHEROOT/sqlite-amalgamation-308301.zip ] ; then
+	try curl -L http://sqlite.org/2014/sqlite-amalgamation-3080301.zip > $CACHEROOT/sqlite-amalgamation-308301.zip
+fi
+if [ ! -d $TMPROOT/apsw ]; then
+	try pushd $TMPROOT
+	try git clone -b master https://github.com/rogerbinns/apsw
+	try cd apsw
+	try unzip $CACHEROOT/sqlite-amalgamation-308301.zip
+	try popd
+fi
+
+pushd $TMPROOT/apsw
+OLD_CFLAGS="$CFLAGS"
+OLD_LDSHARED="$LDSHARED"
+export LDSHARED="$KIVYIOSROOT/tools/liblink"
+export CFLAGS="$CFLAGS -DEXPERIMENTAL -DAPSW_USE_SQLITE_AMALGAMATION=\'sqlite3.c\'"
+
+$HOSTPYTHON setup.py build_ext -v
+$HOSTPYTHON setup.py install -O2
+
+
+export LDSHARED="$OLD_LDSHARED"
+export CFLAGS="OLD_CFLAGS"
+popd
+
+bd=$TMPROOT/apsw/build/lib.macosx-*/
+try $KIVYIOSROOT/tools/biglink $BUILDROOT/lib/apsw.a $bd
+deduplicate $BUILDROOT/lib/libios.a
+
+echo '== APSW build is done'
