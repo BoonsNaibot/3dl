f4a77384eb5eeb44853d8f8f1f496876ab0e71a5
BoonsNaibot <deguzouwa@yahoo.com>
2014-02-26 20:04:44 -0600

    Update build-apsw.sh

--- build-apsw.sh
+++ build-apsw.sh
@@ -6,26 +6,35 @@ if [ ! -f $CACHEROOT/sqlite-amalgamation-3080301.zip ] ; then
 	try curl -L http://sqlite.org/2014/sqlite-amalgamation-3080301.zip > $CACHEROOT/sqlite-amalgamation-3080301.zip
 fi
 if [ ! -d $TMPROOT/apsw ]; then
-        try pushd $TMPROOT
-        try git clone -b master https://github.com/rogerbinns/apsw
-        try cd apsw
-        try unzip $CACHEROOT/sqlite-amalgamation-3080301.zip
-        try mv sqlite-amalgamation-3080301/* $TMPROOT/apsw
-        try rm -rf sqlite-amalgamation-3080301
-        try popd
+	try pushd $TMPROOT
+	try git clone -b master https://github.com/rogerbinns/apsw
+	try cd apsw
+	try unzip $CACHEROOT/sqlite-amalgamation-3080301.zip
+	try mv sqlite-amalgamation-3080301/* $TMPROOT/apsw
+	try rm -rf sqlite-amalgamation-3080301
+	try popd 
 fi
 
 pushd $TMPROOT/apsw
+OLD_CC="$CC"
 OLD_CFLAGS="$CFLAGS"
+OLD_LD="$LD"
+OLD_LDFLAGS="$LDFLAGS"
 OLD_LDSHARED="$LDSHARED"
 export LDSHARED="$KIVYIOSROOT/tools/liblink"
-export CFLAGS="$CFLAGS -DEXPERIMENTAL -DAPSW_USE_SQLITE_AMALGAMATION=\'sqlite3.c\'"
+export LDFLAGS="$ARM_LDFLAGS"
+export LD="$ARM_LD"
+export CFLAGS="$ARM_CFLAGS -DEXPERIMENTAL -DAPSW_USE_SQLITE_AMALGAMATION=\'sqlite3.c\'"
+export CC="$ARM_CC"
 
 $HOSTPYTHON setup.py build_ext -v
 $HOSTPYTHON setup.py install -O2
 
 export LDSHARED="$OLD_LDSHARED"
+export LDFLAGS="$OLD_LDFLAGS"
+export LD="$OLD_LD"
 export CFLAGS="$OLD_CFLAGS"
+export CC="$OLD_CC"
 popd
 
 bd=$TMPROOT/apsw/build/lib.macosx-*/
