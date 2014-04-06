159514beb9022dbd89a8b3e227b3da8f6a0adb0a
BoonsNaibot <deguzouwa@yahoo.com>
2014-04-06 13:46:45 -0500

    Update quickviewscreen.py

--- quickviewscreen.py
+++ quickviewscreen.py
@@ -4,12 +4,14 @@ Created on Jul 27, 2013
 @author: Divine
 '''
 
-from kivy.properties import ListProperty, NumericProperty, StringProperty
+from kivy.properties import ListProperty, NumericProperty, ObjectProperty, StringProperty
+from listitems import QuickViewScreenItem
 from kivy.utils import escape_markup
 from kivy.lang import Builder
 from uiux import Screen_
 
 class QuickViewScreen(Screen_):
+    _item = ObjectProperty(QuickViewScreenItem)
     action_items = ListProperty([])
     page = StringProperty('')
     page_number = NumericProperty(None)
@@ -32,30 +34,29 @@ class QuickViewScreen(Screen_):
             self.action_items = cursor.fetchall()
 
     def on_full_list(self, *args):
-        list_screen = self.manager.get_screen('List Screen')
-        list_screen.page = self.page
-        list_screen.page_number = self.page_number
+        screen = self.manager.get_screen('List Screen')
+        screen.page, screen.page_number = self.page, self.page_number
         kwargs = {'direction': 'left', 'duration': 0.2}
         self.dispatch('on_screen_change', 'List Screen', kwargs)
 
     def _args_converter(self, row_index, an_obj):
         _dict = {'size_hint_y': 0.3,
-                'screen': self}
+                 'screen': self}
         _dict['ix'], _dict['text'], _dict['when'], _dict['why'], _dict['how'] = an_obj
         what = self.format_title(_dict['text'])
         
         if what:
-        	_dict['text'] = what
-        	_dict['markup'] = True
+            _dict['text'] = what
+            _dict['markup'] = True
 
         return _dict
 
     def format_title(self, str):
     	watchwords = self.watchwords
-    	
+
     	for word in watchwords:
-    		if str.startswith(word + ' '):
-    			return '[b]' + word + '[/b]' + escape_markup(str[(len(word)-1):])
+            if str.startswith(word + ' '):
+                return '[b]' + word + '[/b]' + escape_markup(str[(len(word)-1):])
 
 Builder.load_string("""
 #:import NavBar uiux.NavBar
@@ -68,30 +69,33 @@ Builder.load_string("""
     NavBar:
         id: navbar_id
         text: root.page
-        font_size: self.height*.8
+        size_hint: 1, 0.1127
+        font_size: root.width*0.3
+        pos_hint:{'top': 1, 'x': 0}
 
         Button_:
             text: '<D'
             state_color: app.no_color
-            text_color: app.black
-            font_size: self.height*0.6
+            text_color: app.white
+            font_size: self.width*0.5
             font_name: 'breezi_font-webfont.ttf'
             size_hint: 0.18, 1
             pos_hint: {'center_x': 0.08, 'center_y': 0.5}
             on_press: root.dispatch('on_screen_change', 'Pages Screen', {'direction': 'right', 'duration': 0.2})
         Button_:
-            text: 'D'
+            text: 'd>'
             state_color: app.no_color
-            text_color: app.black
-            font_size: self.height*0.8
-            font_name: 'heydings_icons.ttf'
+            text_color: app.white
+            font_size: self.width*0.5
+            font_name: 'breezi_font-webfont.ttf'
             size_hint: 0.18, 1
+            pos_hint: {'center_x': 0.9, 'center_y': 0.5}
             on_press: root.dispatch('on_full_list')
 
     QuickListView:
         data: root.action_items
         args_converter: root._args_converter
-        list_item: QuickViewScreenItem
+        list_item: root._item
         size_hint: 1, 0.8873
         top: navbar_id.y
 """)        
