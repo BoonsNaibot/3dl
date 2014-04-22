c222b38e7e6c3b710cf9ecb86de9f06c783e4441
BoonsNaibot <deguzouwa@yahoo.com>
2014-04-22 14:02:56 -0500

    OCD

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ class ThreeDoListApp(App):
             connection = Connection('db.db')
             cursor = connection.cursor()
             cursor.execute("""
-                           PRAGMA user_version = 1;
+                           PRAGMA user_version=1;
 
                            CREATE TABLE [table of contents](
                            page_number UNSIGNED INTEGER,
