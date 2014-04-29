b04b42ce357028f972f6b9e2a262922562335df4
BoonsNaibot <deguzouwa@yahoo.com>
2014-04-29 15:39:52 -0500

    Renamed args

--- weakreflist.py
+++ weakreflist.py
@@ -22,16 +22,16 @@ class WeakList(list):
     def __getitem__(self, item):
         return self._get_value(list.__getitem__(self, item))
 
-    def __setitem__(self, item, index):
-        return list.__setitem__(self, index, self._get_ref(item))
+    def __setitem__(self, i, value):
+        return list.__setitem__(self, i, self._get_ref(value))
 
     def __getslice__(self, i, j):
         _get_value = self._get_value
         return [_get_value(x) for x in list.__getslice__(self, i, j)] #slow?
         
-    def __setslice__(self, i, j, y):
+    def __setslice__(self, i, j, values):
         _get_ref = self._get_ref
-        return list.__setslice__(self, i, j, (_get_ref(x) for x in y))
+        return list.__setslice__(self, i, j, (_get_ref(x) for x in values))
 
     def __iter__(self, *args, **kwargs):
         for x in list.__iter__(self, *args, **kwargs):
@@ -40,15 +40,15 @@ class WeakList(list):
     def __repr__(self):
         return "WeakList({!r})".format(list(self))
 
-    def append(self, x):
-        list.append(self, self._get_ref(x))
+    def append(self, value):
+        list.append(self, self._get_ref(value))
         
-    def extend(self, l):
+    def extend(self, values):
         _get_ref = self._get_ref
-        list.extend(self, (_get_ref(x) for x in l))
+        list.extend(self, (_get_ref(x) for x in values))
 
-    def insert(self, i, x):
-        list.insert(self, i, self._get_ref(x))
+    def insert(self, i, value):
+        list.insert(self, i, self._get_ref(value))
         
     def count(self, value):
         return list.count(self, self._get_ref(value))
@@ -57,8 +57,8 @@ class WeakList(list):
         while list.__contains__(self, value):
             list.remove(self, self._get_ref(value))
 
-    def index(self, x):
-        return list.index(self, self._get_ref(x))
+    def index(self, value):
+        return list.index(self, self._get_ref(value))
 
-    def pop(self, value=-1):
-        return list.pop(self, value)
+    def pop(self, i=-1):
+        return list.pop(self, i)
