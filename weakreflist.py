7de82ec3ead2446672e953940d9b06cd4bb0fa3c
BoonsNaibot <deguzouwa@yahoo.com>
2014-05-04 13:24:41 -0500

    Python-only version of `WeakList`.

--- weakreflist.py
+++ weakreflist.py
@@ -4,68 +4,61 @@ from weakref import ref
 
 class WeakList(list):
 
-    def _get_value(self, x):
+    def _get_object(self, x):
         if isinstance(x, ref):
             x = x()
         return x
 
     def _get_ref(self, x):
-        try:
-            x = ref(x, self.remove)
-        finally:
-            return x
-            
-    def __init__(self, l=[], **kwargs):
-        _get_ref = self._get_ref
-        list.__init__(self, (_get_ref(x) for x in l), **kwargs)
+        return ref(x, self.remove)
+
+    def __init__(self, items=[]):
+        super(WeakList, self).__init__((self._get_ref(x, self) for x in items))
 
     def __contains__(self, item):
-        return list.__contains__(self, self._get_ref(item))
+        return super(WeakList, self).__contains__(self._get_ref(item, self))
 
     def __getitem__(self, item):
-        return self._get_value(list.__getitem__(self, item))
-
-    def __setitem__(self, i, value):
-        return list.__setitem__(self, i, self._get_ref(value))
+        return self._get_object(super(WeakList, self).__getitem__(item))
 
     def __getslice__(self, i, j):
-        _get_value = self._get_value
-        return [_get_value(x) for x in list.__getslice__(self, i, j)] #slow?
-        
-    def __setslice__(self, i, j, values):
-        _get_ref = self._get_ref
-        return list.__setslice__(self, i, j, (_get_ref(x) for x in values))
+        return [self._get_object(x) for x in super(WeakList, self).__getslice__(i, j)] #slow?
 
-    def __iter__(self, *args, **kwargs):
-        for x in list.__iter__(self, *args, **kwargs):
-            yield self._get_value(x)
-
-    def __reversed__(self, *args, **kwargs):
-        for x in list.__reversed__(self, *args, **kwargs):
-            yield self._get_value(x)
+    def __iter__(self):
+        for x in super(WeakList, self).__iter__():
+            yield self._get_object(x)
 
     def __repr__(self):
         return "WeakList({!r})".format(list(self))
 
-    def append(self, value):
-        list.append(self, self._get_ref(value))
-        
-    def count(self, value):
-        return list.count(self, self._get_ref(value))
-        
-    def extend(self, values):
-        _get_ref = self._get_ref
-        list.extend(self, (_get_ref(x) for x in values))
+    def __reversed__(self, *args, **kwargs):
+        for x in super(WeakList, self).__reversed__(*args, **kwargs):
+            yield self._get_object(x)
 
-    def index(self, value):
-        return list.index(self, self._get_ref(value))
+    def __setitem__(self, i, item):
+        super(WeakList, self).__setitem__(i, self._get_ref(item, self))
 
-    def insert(self, i, value):
-        list.insert(self, i, self._get_ref(value))
+    def __setslice__(self, i, j, items):
+        super(WeakList, self).__setslice__(i, j, (self._get_ref(x, self) for x in items))
 
-    def pop(self, i=-1):
-        return list.pop(self, i)
+    def _remove(self, item):
+        while super(WeakList, self).__contains__(item):
+            super(WeakList, self).remove(item)
 
-    def remove(self, value):
-        while list.__contains__(self, value):
-            list.remove(self, self._get_ref(value))
+    def append(self, item):
+        super(WeakList, self).append(self._get_ref(item, self))
+
+    def count(self, item):
+        return super(WeakList, self).count(self._get_ref(item, self))
+
+    def extend(self, items):
+        super(WeakList, self).extend((self._get_ref(x, self) for x in items))
+
+    def index(self, item):
+        return super(WeakList, self).index(self._get_ref(item, self))
+
+    def insert(self, i, item):
+        super(WeakList, self).insert(i, self._get_ref(item, self))
+
+    def remove(self, item):
+        self._remove(self._get_ref(item, self))
