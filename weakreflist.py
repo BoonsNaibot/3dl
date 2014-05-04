67ad317675187966cc70c5a2ba8c170d2c5af200
BoonsNaibot <deguzouwa@yahoo.com>
2014-05-04 13:28:14 -0500

    No need to pass `self` to `_get_ref` in this one.

--- weakreflist.py
+++ weakreflist.py
@@ -10,13 +10,13 @@ class WeakList(list):
         return x
 
     def _get_ref(self, x):
-        return ref(x, self.remove)
+        return ref(x, self._remove)
 
     def __init__(self, items=[]):
-        super(WeakList, self).__init__((self._get_ref(x, self) for x in items))
+        super(WeakList, self).__init__((self._get_ref(x) for x in items))
 
     def __contains__(self, item):
-        return super(WeakList, self).__contains__(self._get_ref(item, self))
+        return super(WeakList, self).__contains__(self._get_ref(item))
 
     def __getitem__(self, item):
         return self._get_object(super(WeakList, self).__getitem__(item))
@@ -36,29 +36,29 @@ class WeakList(list):
             yield self._get_object(x)
 
     def __setitem__(self, i, item):
-        super(WeakList, self).__setitem__(i, self._get_ref(item, self))
+        super(WeakList, self).__setitem__(i, self._get_ref(item))
 
     def __setslice__(self, i, j, items):
-        super(WeakList, self).__setslice__(i, j, (self._get_ref(x, self) for x in items))
+        super(WeakList, self).__setslice__(i, j, (self._get_ref(x) for x in items))
 
     def _remove(self, item):
         while super(WeakList, self).__contains__(item):
             super(WeakList, self).remove(item)
 
     def append(self, item):
-        super(WeakList, self).append(self._get_ref(item, self))
+        super(WeakList, self).append(self._get_ref(item))
 
     def count(self, item):
-        return super(WeakList, self).count(self._get_ref(item, self))
+        return super(WeakList, self).count(self._get_ref(item))
 
     def extend(self, items):
-        super(WeakList, self).extend((self._get_ref(x, self) for x in items))
+        super(WeakList, self).extend((self._get_ref(x) for x in items))
 
     def index(self, item):
-        return super(WeakList, self).index(self._get_ref(item, self))
+        return super(WeakList, self).index(self._get_ref(item))
 
     def insert(self, i, item):
-        super(WeakList, self).insert(i, self._get_ref(item, self))
+        super(WeakList, self).insert(i, self._get_ref(item))
 
     def remove(self, item):
-        self._remove(self._get_ref(item, self))
+        self._remove(self._get_ref(item))
