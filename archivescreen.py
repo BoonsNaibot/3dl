abf0e3e430490607c56c15bc887d8ca8d8610f81
BoonsNaibot <deguzouwa@yahoo.com>
2014-05-05 16:27:10 -0500

    Makin' class attributes

--- archivescreen.py
+++ archivescreen.py
@@ -28,9 +28,7 @@ class ArchiveScreen(Screen_):
             self.list_items = cursor.fetchall()
 
     def _args_converter(self, row_index, an_obj):
-        _dict = {'content_height_hint': (322./1136.),
-                 'aleft': True,
-                 'screen': self.proxy_ref}
+        _dict = {'content_height_hint': (322./1136.)}
         _dict['ix'], _dict['text'], _dict['when'], _dict['why'], _dict['how'] = an_obj
         _dict['why'] = bool(_dict['why'])
         return _dict
